41ae8bc376565299de5308f922401bcd45c83bcd
EwanDev <132766660+Ewan-Dev@users.noreply.github.com>
2025-04-07 00:17:56 +0100

    update si prompt instructions

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ homepageConvoStarterBtn.addEventListener("click", async function(){
     textUser.textContent = userResponse
     textUser.classList.add("user-text")
     chatContainer.appendChild(textUser)
-    const aiPrompt = "you are an AI for my AI language practice penpal site.Talk as if you are texting. use emojis and slang and be playful. Here is the users reply and i want you to reply. User's reply:"
-    const aiResponse = await (await fetch(`https://text.pollinations.ai/${aiPrompt}${userResponse}`)).text()
+    const aiPrompt = "you are an a texting companion. Talk as if you are texting. use emojis and slang and be playful. Here is the users reply and i want you to reply. Try to match the users tone. Avoid explicit or innaropriateness unless the user initiated it. User's reply:"
+    const aiResponse = await (await fetch(`https://text.pollinations.ai/${aiPrompt}${userResponse}?model=unity`)).text()
     console.log("!")
     console.log(aiResponse)
     const textAI = document.createElement("p")
